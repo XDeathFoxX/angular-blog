b2c400be51d3a0d8a6ab7de418dde45f4b3fa629
Valdemir Junior <valdemirvoltatoni@gmail.com>
2023-11-25 20:10:18 -0300

    firstUpdate

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-big-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+export class BigCardComponent {
+
+}
